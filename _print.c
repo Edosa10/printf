6c520e7b70ea232e995cb9681122493dc17c1996
Ehiorobo christopher Edosa <ehiorobo.chris@gmail.com>
2023-08-25 11:34:17 -0700

    add

--- _print.c
+++ _print.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -6,53 +5,53 @@
  *
  * Return: Always 0
  */
+int main(void);
 
 int _printf(const char *format, ...) {
     va_list args;
     va_start(args, format);
-    
-    int count = 0;  // To keep track of the number of characters printed
-    
+
+    int count = 0; // To keep track of the number of characters printed
+
     while (*format) {
-        if (*format != '%') {
-            // Ordinary character, just write it
-            write(1, format, 1);
-            count++;
-        } else {
-            format++;  // Move past the '%'
-            
-            // Check the conversion specifier
-            switch (*format) {
-                case 'c':
-                    // Print a character
-                    char c = va_arg(args, int);
-                    write(1, &c, 1);
+        if (*format == '%') {
+            format++; // Move past the '%'
+            if (*format == '\0') {
+                // If '%' is the last character in the format, break the loop
+                break;
+            } else if (*format == 'c') {
+                // Handle %c conversion specifier
+                char c = va_arg(args, int);
+                putchar(c);
+                count++;
+            } else if (*format == 's') {
+                // Handle %s conversion specifier
+                char *str = va_arg(args, char*);
+                while (*str) {
+                    putchar(*str);
+                    str++;
                     count++;
-                    break;
-                case 's':
-                    // Print a string
-                    char *str = va_arg(args, char *);
-                    for (int i = 0; str[i]; i++) {
-                        write(1, &str[i], 1);
-                        count++;
-                    }
-                    break;
-                case '%':
-                    // Print a literal '%'
-                    write(1, "%", 1);
-                    count++;
-                    break;
-                default:
-                    // Unknown conversion specifier, just print the '%'
-                    write(1, "%", 1);
-                    count++;
-                    break;
+                }
+            } else if (*format == '%') {
+                // Handle %% conversion specifier (print a single '%')
+                putchar('%');
+                count++;
             }
+        } else {
+            // Regular character, just print it
+            putchar(*format);
+            count++;
         }
-        
-        format++;  // Move to the next character in the format string
+
+        format++;
     }
-    
+
     va_end(args);
     return count;
 }
+
+int main() {
+    _printf("Hello, %s! The answer is %d%%. A char: %c\n", "world", 42, 'A');
+    return 0;
+}
+
